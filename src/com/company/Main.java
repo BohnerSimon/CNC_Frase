95920ed2330831aae7e4f0effda1b5daeace9a0c
BohnerSimon <Ui\1U!@#$S:t,E%cR9>
2020-06-18 13:52:30 +0200

    Merging1
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        System.out.println("BLABLBALa");
        System.out.println("Test");
        System.out.println(1+1);
     }
