edd43f02684bb2e61c4ca4eaa4dbd131379c08d7
PhineX98 <s191584@student.dhbw-mannheim.de>
2020-06-24 16:30:56 +0200

    testklasse delete befehlsCode erstellt main gecleared

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("BLABLBALa");
-       System.out.println("Test");
-       System.out.println(1+1);
-       System.out.println("eyyyyy");
+
     }
 }
+
---
+++ src/com/company/befehlsCode.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class befehlsCode {
+    String pos;
+    String befehl;
+    double x;
+    double y;
+    double i;
+    double j;
+
+    public befehlsCode(String pos, String befehl, double x, double y, double i, double j) {
+        this.pos = pos;
+        this.befehl = befehl;
+        this.x = x;
+        this.y = y;
+        this.i = i;
+        this.j = j;
+    }
+}
