1bad0f803c0f25d6c6319aea893dd7a7b654c361
philippmayer01 <67101493+philippmayer01@users.noreply.github.com>
2020-06-24 15:52:41 +0200

    Import von json.simple

--- src/com/company/ReadInJson.java
+++ src/com/company/ReadInJson.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class ReadInJson {
 
 	// Author: Philipp Mayer
