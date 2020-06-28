af4a0154be3db8c749cab8fcab5d33e9f1250aae
philippmayer01 <67101493+philippmayer01@users.noreply.github.com>
2020-06-28 13:30:37 +0200

    Beispiel
    Hier könnt ihr beispielhaft sehen, wie wir den Dateipfad abfragen
    könnten, was dann in unserer GUI im Eingabefeld passieren müsste. Diesen
    Pfad könnte man dann der readJson()-Methode übergeben, um die Befehle
    der Datei abzuspeichern und weiteren Methoden übergeben.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
    public static void main(String[] args) {
 	// write your code here
        
+	   ReadInJson json = new ReadInJson();
+	   
+	   Scanner scanner = new Scanner(System.in);
+	   
+	   System.out.println("Geben sie bitte den Dateipfad der JSON-Datei an.");
+	   String path = scanner.nextLine();
+	   
+	   //"C:\\Users\\Philipp.mayer\\Desktop\\Befehlscode.json"
+	   
+	   json.readJson(path);
+	   
+	   
+	   
+	   
+	   
+	   
+	   
 
     }
 }
